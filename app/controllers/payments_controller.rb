1b3b92147051d61bc3fb16d14c87a8d4160019f2
Lina Meyer <linammeyer@gmail.com>
2018-05-31 17:12:49 +0200

    payment show page works now

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -13,7 +13,7 @@ class PaymentsController < ApplicationController
       charge = Stripe::Charge.create(
         customer:     customer.id,   # You should store this customer id and re-use it.
         amount:       @order.amount_cents,
-        description:  "Payment for book #{@order.book_sku} for order #{@order.id}",
+        description:  "Payment for book #{@order.personalization_sku} for order #{@order.id}",
         currency:     @order.amount.currency
       )
 
