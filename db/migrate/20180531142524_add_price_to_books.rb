3c12a48c4aff2438f0470dbd9f8ebb1e5a1d11f4
Dania Maciel <daniaemily@gmail.com>
2018-06-01 15:07:28 +0200

    Enabled Price to Books field

--- db/migrate/20180531142524_add_price_to_books.rb
+++ db/migrate/20180531142524_add_price_to_books.rb
@@ -1,6 +1,6 @@
 class AddPriceToBooks < ActiveRecord::Migration[5.2]
   def change
-    # add_monetize :books, :price, currency: { present: false }
+    add_monetize :books, :price, currency: { present: false }
     # disabled because Dania already had a monetize field
   end
 end
