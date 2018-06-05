7003fd4a691c6de7c6d78b970d5e9573d23431ad
Lina Meyer <linammeyer@gmail.com>
2018-06-05 20:59:43 +0200

    changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
+Favorite.destroy_all
+Personalization.destroy_all
+Page.destroy_all
 Book.destroy_all
 Review.destroy_all
-Personalization.destroy_all
 
 User.create(email: "test@test.com", password: "123123")
 
