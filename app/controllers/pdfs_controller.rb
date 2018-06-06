9637c0da145fcb33106edbf562b1b2f8c3e9a26d
Lina Meyer <linammeyer@gmail.com>
2018-06-06 12:50:26 +0200

    changed pdf name

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -3,7 +3,7 @@ class PdfsController < ApplicationController
     respond_to do |format|
       format.html
       format.pdf do
-        render pdf: "your_book.pdf",
+        render pdf: "your_book",
         template: "pdfs/show.html.erb",
         layout:'pdf'
       end
