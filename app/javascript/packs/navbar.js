067d2e05e7592759a1d5a5b2a9d67a46994db9b4
Simon Sohm <Simon.Sohm@carini.at>
2018-06-07 10:21:24 +0200

    worked on flexible navbar

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
   $(window).scroll(function(){
     var scroll = $(window).scrollTop();
-    if (scroll > 1063 & scroll < 2190) {
+    if (scroll > $(window).height()) {
       $(".navbar-bookie").css("background" , "#9ACFE1");
     }
-    else if (scroll > 2190) {
-      $(".navbar-bookie").css("background" , "#fdbd6a");
-    }
+    // else if (scroll > 2190) {
+    //   $(".navbar-bookie").css("background" , "#fdbd6a");
+    // }
     else{
       $(".navbar-bookie").css("background" , "transparent");
     }
